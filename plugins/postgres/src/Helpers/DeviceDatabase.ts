e1cdf8c5e05fcf1b27cb914614309910de44b538
Bruno Gelatti <bgelatti@gmail.com>
2024-11-07 14:20:06 -0300

    Adding functions to postgres

--- plugins/postgres/src/Helpers/DeviceDatabase.ts
+++ plugins/postgres/src/Helpers/DeviceDatabase.ts
@@ -21,6 +21,7 @@ export async function getDeviceConnection(id: TGenericID, type: TDeviceType) {
       table.timestamp("created_at").nullable();
       table.timestamp("chunk_timestamp_start");
       table.timestamp("chunk_timestamp_end");
+      table.string("serie", 100);
 
       if (type === "mutable") {
         table.timestamp("updated_at");
