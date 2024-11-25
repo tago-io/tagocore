bfbadfed8bc24df4a5de0e3374e23a93cd166fb3
Bruno Gelatti <bgelatti@gmail.com>
2024-11-25 17:27:32 -0300

    Correction of null values]

--- plugins/tagoio-integration/src/front/Auth/Auth.tsx
+++ plugins/tagoio-integration/src/front/Auth/Auth.tsx
@@ -11,8 +11,7 @@ import Otp from "./Otp/Otp.tsx";
 import OtpPicker from "./OtpPicker/OtpPicker.tsx";
 import Profiles from "./Profiles/Profiles.tsx";
 
-// const API_URL = process.env.TAGOIO_API;
-const API_URL = "https://api-tagocore.beta.tago.io";
+const API_URL = process.env.TAGOIO_API;
 
 /**
  * Props.
