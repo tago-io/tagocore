54da67b2c0fec184280712f24ef91520b56a12f2
Bruno Gelatti <bgelatti@gmail.com>
2025-03-25 14:46:59 -0300

    Fixed to mysql to do the migration to 1.0.0

--- plugins/mysql/src/Migrations/20220407173306_0.3.3.ts
+++ plugins/mysql/src/Migrations/20220407173306_0.3.3.ts
@@ -4,29 +4,53 @@ import type { Knex } from "knex";
  * Bumps up to version 0.3.3.
  */
 export async function up(knex: Knex) {
-  await knex.schema.table("device_token", (table) => {
-    table
-      .foreign("device_id")
-      .references("id")
-      .inTable("device")
-      .onDelete("CASCADE");
-  });
+  await knex.schema
+    .raw(
+      `SELECT * FROM information_schema.REFERENTIAL_CONSTRAINTS WHERE REFERENCED_TABLE_NAME = 'device' and table_name = 'device_token'`,
+    )
+    .then(async (data) => {
+      if (data[0].length === 0) {
+        await knex.schema.table("device_token", (table) => {
+          table
+            .foreign("device_id")
+            .references("id")
+            .inTable("device")
+            .onDelete("CASCADE");
+        });
+      }
+    });
 
-  await knex.schema.table("device_params", (table) => {
-    table
-      .foreign("device_id")
-      .references("id")
-      .inTable("device")
-      .onDelete("CASCADE");
-  });
+  await knex.schema
+    .raw(
+      `SELECT * FROM information_schema.REFERENTIAL_CONSTRAINTS WHERE REFERENCED_TABLE_NAME = 'device' and table_name = 'device_params'`,
+    )
+    .then(async (data) => {
+      if (data[0].length === 0) {
+        await knex.schema.table("device_params", (table) => {
+          table
+            .foreign("device_id")
+            .references("id")
+            .inTable("device")
+            .onDelete("CASCADE");
+        });
+      }
+    });
 
-  await knex.schema.table("analysis_log", (table) => {
-    table
-      .foreign("analysis_id")
-      .references("id")
-      .inTable("analysis")
-      .onDelete("CASCADE");
-  });
+  await knex.schema
+    .raw(
+      `SELECT * FROM information_schema.REFERENTIAL_CONSTRAINTS WHERE REFERENCED_TABLE_NAME = 'analysis' and table_name = 'analysis_log'`,
+    )
+    .then(async (data) => {
+      if (data[0].length === 0) {
+        await knex.schema.table("analysis_log", (table) => {
+          table
+            .foreign("analysis_id")
+            .references("id")
+            .inTable("analysis")
+            .onDelete("CASCADE");
+        });
+      }
+    });
 }
 
 /**
