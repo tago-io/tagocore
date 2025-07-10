03fac21f5091534f013289a3f44266fa04a5e05a
Bruno Gelatti <bgelatti@gmail.com>
2025-07-10 09:01:05 -0300

    changes requested in review

---
+++ plugins/tagoio-integration/src/front/Auth/regions.ts
@@ -0,0 +1,11 @@
+// Region configuration
+export const REGIONS = {
+  "us-e1": {
+    label: "United States East 1",
+    apiUrl: "https://api.tago.io",
+  },
+  "eu-w1": {
+    label: "Europe West 1",
+    apiUrl: "https://api.eu-w1.tago.io",
+  },
+};
